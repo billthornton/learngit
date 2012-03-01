d0732b183316d24c6e585cb0c0d491558d55361f
Bill Thornton <billthornton@gmail.com>
2012-03-01 23:44:40 +0000

    Changed how the temp directory is calculated, still needs some improvements

--- lessons/beginner/adding-unadding-changes/setup.sh
+++ lessons/beginner/adding-unadding-changes/setup.sh
@@ -1,9 +1,9 @@
 LESSON_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $LESSON_DIR
+cd $LESSON_DIR
 
 # Create a fake repo that we can clone from
-LESSON_TEMP="/tmp/lessons/$( dirname "${BASH_SOURCE[0]}" )"
-LESSON_TEMP="$(readlink -f $LESSON_TEMP)"
+LESSON_TEMP=`python -c "import os;print os.path.realpath(os.path.join('/tmp/lessons', os.path.basename(os.getcwd())))"`
 echo $LESSON_TEMP
 
 mkdir -p $LESSON_TEMP
@@ -13,7 +13,7 @@ mkdir files
 cp -R $LESSON_DIR/files $LESSON_TEMP/files
 cd $LESSON_TEMP/workspace/__lesson_name__
 git init
-cp -R $LESSON_TEMP/files/1 $LESSON_TEMP/workspace/__lesson_name__
+cp -R $LESSON_DIR/files/1/ $LESSON_TEMP/workspace/__lesson_name__
 git add templates
 git commit -m "Initial commit"
 
--- lessons/beginner/basic-workflow/setup.sh
+++ lessons/beginner/basic-workflow/setup.sh
@@ -1,8 +1,9 @@
 LESSON_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $LESSON_DIR
+cd $LESSON_DIR
+
 # Create a fake repo that we can clone from
-LESSON_TEMP="/tmp/lessons/$( dirname "${BASH_SOURCE[0]}" )"
-LESSON_TEMP="$(readlink -f $LESSON_TEMP)"
+LESSON_TEMP=`python -c "import os;print os.path.realpath(os.path.join('/tmp/lessons', os.path.basename(os.getcwd())))"`
 echo $LESSON_TEMP
 
 mkdir -p $LESSON_TEMP
